f99ba3d4402ec941137814eb072ca5fe1d60d892
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-02-28 21:26:40 +0700

    added constants.go for learning constant declaration

---
+++ constants.go
@@ -0,0 +1,16 @@
+// MARK: - Part 5: Constants
+// Reference: https://golangbot.com/constants/
+
+package main
+
+import "fmt"
+
+func main() {
+
+	const citizenID = 75333123944123123
+	const firstName = "Arifin"
+	const lastName = "Firdaus"
+
+	fmt.Println("full name: ", firstName, lastName, "has ID of", citizenID)
+
+}
